cf8d586894ef74390924221e8c84376de1eccb0c
Aldon <aisenberg@freightview.com>
2018-08-05 16:21:45 -0500

    create vuex store, retrieving items from vuex store

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "comma-dangle": ["error", "never"],
-    "semi": ["error", "never"]
+    "semi": ["error", "never"],
+    "no-multi-spaces": "off"
 
   },
   parserOptions: {
