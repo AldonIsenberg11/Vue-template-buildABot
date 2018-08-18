3073cb78ef1fb6604df93efff27c70f546f6e7c5
Aldon <aisenberg@freightview.com>
2018-08-18 12:11:30 -0500

    use actions to save data to an API

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,13 @@ module.exports = {
     '@vue/airbnb'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "comma-dangle": ["error", "never"],
-    "semi": ["error", "never"],
-    "no-multi-spaces": "off",
-    "no-param-reassign": 0
+    'no-console'           : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger'          : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "comma-dangle"         : ["error", "never"],
+    "semi"                 : ["error", "never"],
+    "no-multi-spaces"      : "off",
+    "no-param-reassign"    : 0,
+    "object-curly-spacing" : 0
 
   },
   parserOptions: {
